d86e343ecbd7c86e89fc6954791f0d8acdae964e
Sergio Martínez Cid <martinezcidsergio@gmail.com>
2021-12-20 19:48:57 +0100

    Add compulsory features (WIP)

--- src/chatbot_agent.py
+++ src/chatbot_agent.py
@@ -1,8 +1,12 @@
 import json
+import re
+from time import gmtime, strftime
 from spade import agent
 from spade.behaviour import CyclicBehaviour, OneShotBehaviour
 from spade.message import Message
 from spade.template import Template
+
+from const import ENVIRONMENT_FOLDER
 
 class ChatbotAgent(agent.Agent):
     def __init__(self, jid, password, verify_security=False):
@@ -28,17 +32,69 @@ class SendGreetingBehaviour(OneShotBehaviour):
         await self.send(message)
 
 class HandleRequestsBehaviour(CyclicBehaviour):
+    available_queries = {
+        re.compile(r'\s*show\s*me\s*the\s*time\s*', re.I):
+            (lambda _: ShowTimeBehaviour()),
+        re.compile(r'who\s*is\s*(.*)\s*', re.I):
+            (lambda name: SearchPersonInfoBehaviour(name[0])),
+        re.compile(r'\s*create\s*file\s*named\s*(.*)\s*', re.I):
+            (lambda name: MakeFileBehaviour(name[0])),
+        re.compile(r'\s*exit\s*', re.I):
+            (lambda _: SendExitBehaviour()),
+    }
+
     async def run(self):
         message = await self.receive(10000)
         if message is None:
             return
-        action = None
-        if message.body.strip().lower() == 'exit':
-            action = SendExitBehaviour()
-        else :
-            action = NotUnderstoodBehaviour()
+        action = self.get_response_from_message(message.body)
         self.agent.add_behaviour(action)
         await action.join()
+
+    def get_response_from_message(self, message):
+        for query in self.available_queries:
+            match = query.match(message)
+            if match is not None:
+                return self.available_queries[query](match.groups())
+        return NotUnderstoodBehaviour()
+
+class ShowTimeBehaviour(OneShotBehaviour):
+    async def run(self):
+        message = Message(to=self.agent.user_address)
+        message.set_metadata('performative', 'inform')
+        message.set_metadata('language', 'chatbot-response')
+        message.body = 'The time is ' + strftime("%d-%m-%Y %H:%M:%S", gmtime())
+        await self.send(message)
+
+class SearchPersonInfoBehaviour(OneShotBehaviour):
+    def __init__(self, name):
+        super().__init__()
+        self.name = name
+
+    async def run(self):
+        message = Message(to=self.agent.user_address)
+        message.set_metadata('performative', 'inform')
+        message.set_metadata('language', 'chatbot-response')
+        message.body = 'who ' + self.name
+        await self.send(message)
+
+class MakeFileBehaviour(OneShotBehaviour):
+    def __init__(self, name):
+        super().__init__()
+        self.name = name
+
+    async def run(self):
+        try:
+            with open(f'{ENVIRONMENT_FOLDER}/{self.name}', 'a', encoding='utf-8'):
+                pass
+            message_body = f'Successfully created \'{self.name}\''
+        except OSError as e:
+            message_body = e.strerror
+        message = Message(to=self.agent.user_address)
+        message.set_metadata('performative', 'inform')
+        message.set_metadata('language', 'chatbot-response')
+        message.body = message_body
+        await self.send(message)
 
 class SendExitBehaviour(OneShotBehaviour):
     async def run(self):

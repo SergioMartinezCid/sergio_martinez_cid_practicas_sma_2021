c53181d4e167c16507d5d81b3720d2062f7b72be
Sergio Martínez Cid <martinezcidsergio@gmail.com>
2021-12-28 12:54:29 +0100

    Ensure seeded data is unique

--- src/database/functionality_regex.py
+++ src/database/functionality_regex.py
@@ -1,15 +1,12 @@
 from sqlalchemy import Column, String, Enum
-from sqlalchemy.sql.sqltypes import Integer
 from functionality import Functionality
 from .base import Base
 
 class FunctionalityRegex(Base):
     __tablename__ = "functionality_regex"
 
-    id  = Column(Integer, primary_key=True, autoincrement=True)
-    regex = Column(String, unique=True)
+    regex = Column(String, primary_key=True)
     functionality = Column(Enum(Functionality), nullable=False)
 
     def __repr__(self) -> str:
-        return f'FunctionalityRegex(id={self.id!r}, regex={self.url!r}), ' + \
-            'functionality={self.functionality!r}'
+        return f'FunctionalityRegex(regex={self.url!r}), functionality={self.functionality!r}'
--- src/database/joke.py
+++ src/database/joke.py
@@ -1,12 +1,10 @@
 from sqlalchemy import Column, String
-from sqlalchemy.sql.sqltypes import Integer
 from .base import Base
 
 class Joke(Base):
     __tablename__ = "joke"
 
-    id  = Column(Integer, primary_key=True, autoincrement=True)
-    joke = Column(String, unique=True, nullable=False)
+    joke = Column(String, primary_key=True)
 
     def __repr__(self) -> str:
-        return f'Joke(id={self.id!r}, joke={self.url!r})'
+        return f'Joke(joke={self.url!r})'
